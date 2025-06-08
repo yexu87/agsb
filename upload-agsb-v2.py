2255d546ecfc6aabf53ef33a0faf9555985ffa1c
yexu87 <yexu87@gmail.com>
2025-06-08 18:52:16 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,7 +31,7 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "rregreg"         # 用户名
+USER_NAME = "rregregg"         # 用户名
 UUID = "16fea94d-fa7e-46d1-acc9-9996a7eafe77"                     # UUID，留空则自动生成
 PORT = 59999                   # Vmess端口，留空或0则自动生成
 DOMAIN = "suidao.xddx.dpdns.org"                   # 域名，留空则自动获取
